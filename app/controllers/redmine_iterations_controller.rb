e6b40e8f2c17a92daa820b261efed96d24ac50a8
amr-abdelhamid <amr@agileacademy.co>
2016-08-29 15:29:07 +0300

    fixed a display bug: displayed version is always not the latest

--- app/controllers/redmine_iterations_controller.rb
+++ app/controllers/redmine_iterations_controller.rb
@@ -9,11 +9,12 @@ class RedmineIterationsController < ApplicationController
 
   def index
   	sort_init 'version.start_date', 'desc'
-  	sort_update %w(projects.name vers ions.name versions.start_date versions.effective_date)
+  	sort_update %w(projects.name versions.name versions.start_date versions.effective_date)
 
-  	@projects = Project.joins("LEFT JOIN versions on projects.id = versions.project_id and versions.start_date = (select MAX(versions.start_date) from versions where versions.project_id = projects.id)")
-  		.where("projects.status =  #{Project::STATUS_ACTIVE} ")
-      .where("UCase(versions.name) like 'ITRATION%' or versions.name like 'SPRINT%'")
-		.order(sort_clause)
+  	@projects = Project.joins("LEFT JOIN versions on (projects.id = versions.project_id " +
+        "and versions.start_date = (select MAX(v.start_date) from versions v where v.project_id = projects.id) " +
+        "and (UCASE(versions.name) like 'ITER%' or UCASE(versions.name) like 'SPRINT%'))")
+  		.where("projects.status =  #{Project::STATUS_ACTIVE}")
+		  .order(sort_clause)
   end
 end
