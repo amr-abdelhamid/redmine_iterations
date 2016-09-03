39b46c1a34f051ca4947af6654aa52d497b8e5cd
amr-abdelhamid <amr@agileacademy.co>
2016-09-03 10:31:57 +0300

    fixed invalid results in case of later versions defined which are not iterations

--- app/controllers/redmine_iterations_controller.rb
+++ app/controllers/redmine_iterations_controller.rb
@@ -11,18 +11,29 @@ class RedmineIterationsController < ApplicationController
   	sort_init 'version.start_date', 'desc'
   	sort_update %w(projects.name versions.name versions.start_date versions.effective_date)
 
-  	@projects = Project.joins("LEFT JOIN versions on (projects.id = versions.project_id " +
-        "and versions.start_date = (select MAX(v.start_date) from versions v where v.project_id = projects.id) " +
-        "and (UCASE(versions.name) like 'ITER%' or UCASE(versions.name) like 'SPRINT%'))")
-  		.where("projects.status =  #{Project::STATUS_ACTIVE}")
-		  .order(sort_clause)
+    projects = Project.includes(:versions)
+      .where("projects.status =  #{Project::STATUS_ACTIVE}")
+      .order(sort_clause)
 
-    # #remove unimportant projects
-    # @projects.delete_if {|project| project.custom_fields.contain < 3}
-    # @projects.each do |project|
-    #   if project.custom_fields.include("type")
-    #
-    #   end
-    # end
+    @projects_and_last_iteration = Hash.new
+    temp_iteration = nil
+    projects.each do |temp_project|
+      @projects_and_last_iteration[temp_project] = get_last_iteration_for_project(temp_project)
+    end
+
+  end
+
+  private
+
+  def get_last_iteration_for_project(project)
+    last_iteration = nil
+    project.versions.each do |v|
+      if(v.name.downcase.include?('iteration') || v.name.downcase.include?('sprint'))
+        if(last_iteration == nil || v.start_date > last_iteration.start_date)
+          last_iteration = v
+        end
+      end
+    end
+    return last_iteration
   end
 end
